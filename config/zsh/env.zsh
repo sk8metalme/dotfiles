a7479326026dfe0e85697527516ed5a44ca5b603
hohi <sk8metal@me.com>
2025-12-31 22:50:36 +0900

    Add HOME/bin to the PATH environment variable

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -43,3 +43,8 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 # bun completions
 [ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
 
+
+# ------------------------------------------------------------------------
+# other
+# ------------------------------------------------------------------------
+export PATH="$HOME/bin:$PATH"
